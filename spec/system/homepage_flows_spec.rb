c8dd4f9b7f36abf80d69254aae72c34f6c2a9367
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2023-11-23 22:11:46 +0100

    test: example system spec

---
+++ spec/system/homepage_flows_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "HomepageFlows", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it "works! (now write some real specs)" do
+    visit "/"
+    expect(page).to have_css('.logo')
+  end
+end

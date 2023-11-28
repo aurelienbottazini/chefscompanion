56f605a77460562eaa853b7122e5702e158c62ab
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2023-11-28 20:44:49 +0100

    feat: add recipe ingredients to display popup

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -43,6 +43,36 @@ function SearchBar({handleSearch}) {
 }
 
 function RecipeDisplay({recipe, dialogRef}) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [ingredients, setIngredients] = useState([]);
+
+    useEffect(() => {
+        if (!recipe.id) return;
+
+        const fetchRecipeIngredients = async () => {
+            setIsLoading(true);
+            setError(null);
+
+            try {
+
+                const ingredients_response = await fetch(`/recipe_ingredients/show?id=${recipe.id}`)
+
+                if (!ingredients_response.ok) {
+                    throw new Error('Error fetching recipes, network response is invalid.')
+                }
+
+                ingredients_response.json().then((x) => setIngredients(x));
+
+            } catch (error) {
+                setError(error.message)
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchRecipeIngredients();
+    }, [recipe.id])
 
     const closeDialog = () => {
         dialogRef.current.close();
@@ -59,9 +89,7 @@ function RecipeDisplay({recipe, dialogRef}) {
 
                 </ul>
                 <ul id="ingredients">
-                    <li>Prep: {recipe.prep_time}</li>
-                    <li>Cook: {recipe.cook_time}</li>
-                    <li>Ratings: {recipe.ratings}</li>
+                    {ingredients.map(x => <li key={x.id}>{x.full_text}</li>)}
                 </ul>
                 <button onClick={closeDialog}>Close X</button>
             </section>

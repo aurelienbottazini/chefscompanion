26cd0b98815fd431928237ad850590eaef0f8f44
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2023-12-03 18:32:04 +0100

    test: add specs for homepage and ingredients_search

--- spec/requests/homepage_spec.rb
+++ spec/requests/homepage_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe "Homepages", type: :request do
     it "returns http success" do
       get "/homepage/index"
       expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq("text/html; charset=utf-8")
+      expect(response.body).to include("/assets/chefscompanion")
     end
   end
-
 end
--- spec/requests/ingredients_search_spec.rb
+++ spec/requests/ingredients_search_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "IngredientsSearches", type: :request do
   describe "GET /create" do
-    it "returns http success" do
-      get "/ingredients_search/create"
-      expect(response).to have_http_status(:success)
+    context "with no ingredients" do
+      it "returns http success" do
+        get "/ingredients_search/create"
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+        expect(JSON.parse(response.body)["recipes"]).to eq([])
+      end
     end
   end
-
 end

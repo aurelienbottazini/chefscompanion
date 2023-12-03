64eb3f3f137100985e311bdd76399f5c817f5f03
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2023-12-03 18:44:58 +0100

    test: ingredients_search_spec.rb

--- spec/requests/ingredients_search_spec.rb
+++ spec/requests/ingredients_search_spec.rb
@@ -10,5 +10,16 @@ RSpec.describe "IngredientsSearches", type: :request do
         expect(JSON.parse(response.body)["recipes"]).to eq([])
       end
     end
+
+    context "with ingredients" do
+      let!(:recipe) { create(:recipe)}
+
+      it "returns http success" do
+        get "/ingredients_search/create?ingredients=flour"
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+        expect(JSON.parse(response.body)["recipes"].length).to eq(1)
+      end
+    end
   end
 end

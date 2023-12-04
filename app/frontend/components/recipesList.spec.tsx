111993e6ba2d49ea85c17967aac1c2c8a8044e69
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2023-12-04 08:48:13 +0100

    test: react renders a list of recipe

--- app/frontend/components/recipesList.spec.tsx
+++ app/frontend/components/recipesList.spec.tsx
@@ -7,4 +7,14 @@ describe(RecipesList, () => {
     it('renders', () => {
         render(<RecipesList recipes={[]} dialogRef={null} setRecipe={() => {}}  />)
     })
+
+    it('renders a list of recipes', () => {
+        const recipes = [
+            {id: 1, title: 'Recipe 1'},
+            {id: 2, title: 'Recipe 2'},
+        ]
+        const {getByText} = render(<RecipesList recipes={recipes} dialogRef={null} setRecipe={() => {}}  />)
+        expect(getByText('Recipe 1')).toBeTruthy()
+        expect(getByText('Recipe 2')).toBeTruthy()
+    })
 });
